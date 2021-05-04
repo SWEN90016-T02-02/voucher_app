e9d402e2b8bdccc0331a93fd808a6f0b0ebff121
xhhh_chen <kirikirimendes@gmail.com>
2021-05-04 13:51:09 +1000

    User schema fixed

--- appbackend/models/user.js
+++ appbackend/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     first_name: {type: String, required: true},
     last_name : {type: String,  required: true},
+    phone_number : {type: String,  required: true},
     user_id : {type: Number, min : 0, required: true},
     email_address: {type: String,  required: true},
     password: {type: String,  required: true},   
