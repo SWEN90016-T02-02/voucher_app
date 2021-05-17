7d6e9839a951df1c1e40e1d4d8d6a04997ecc503
黄震扬 <hzy@huangzhangdeMBP.lan>
2021-05-17 20:26:34 +1000

    minor fix

--- appbackend/controllers/voucherControllers.js
+++ appbackend/controllers/voucherControllers.js
@@ -12,10 +12,10 @@ const redeemVoucher = async (req,res) => {
             const voucher_point = voucher.point
             // the name of the object in the body should be the same as what 
             // it is in the input's name field within the form
-            const user = await User.find({email_address:req.body.email}).lean()
+            const user = await User.find({email:req.body.email}).lean()
             if (user){
                 var user_point = user.point + voucher_point
-                User.updateOne({email_address:req.body.email},{$set:{point:user_point}})
+                User.updateOne({email:req.body.email},{$set:{point:user_point}})
                 res.redirect('/profile') // ---awaiting changes--- 
             }
             else{
